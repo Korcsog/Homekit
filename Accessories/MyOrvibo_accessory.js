a88f67f87142771e1940c453f5f2492ab5fe1f3d
Kristóf <kristof.korcsog@t-online.hu>
2017-02-01 08:21:39 +0100

    Orvibo S20 Homekit support

---
+++ Accessories/MyOrvibo_accessory.js
@@ -0,0 +1,88 @@
+var Accessory = require('../').Accessory;
+var Service = require('../').Service;
+var Characteristic = require('../').Characteristic;
+var uuid = require('../').uuid;
+var err = null; // in case there were any problems
+var cmd=require("node-cmd")
+
+
+// here's a fake hardware device that we'll expose to HomeKit
+var OUTLET = {
+  powerOn: false,
+    setPowerOn: function(on) {
+    console.log("Turning the outlet %s!...", on ? "on" : "off");
+    if (on) {
+      
+          OUTLET.powerOn = on;  // =true volt
+      cmd.run('cd /home/pi/HAP-NodeJS/Orvibo/ && sudo python orvibo.py -i 192.168.1.82 -s on'); // orvibo's IP address    
+          console.log("...outlet is now on.");
+    } 
+    else {
+     
+          console.log("...outlet is now off.");
+          OUTLET.powerOn = false;
+           cmd.run('cd /home/pi/HAP-NodeJS/Orvibo/ && sudo python orvibo.py -i 192.168.1.82 -s off');
+          }
+  },
+    identify: function() {
+    console.log("Identify the outlet.");
+    }
+}
+
+// Generate a consistent UUID for our outlet Accessory that will remain the same even when
+// restarting our server. We use the `uuid.generate` helper function to create a deterministic
+// UUID based on an arbitrary "namespace" and the accessory name.
+var outletUUID = uuid.generate('hap-nodejs:accessories:Outlet');
+
+// This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
+var outlet = exports.accessory = new Accessory('Outlet', outletUUID);
+
+// Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
+outlet.username = "AA:BB:CC:4D:5D:10";
+outlet.pincode = "031-45-154";
+
+// set some basic properties (these values are arbitrary and setting them is optional)
+outlet
+  .getService(Service.AccessoryInformation)
+  .setCharacteristic(Characteristic.Manufacturer, "Orvibo")
+  .setCharacteristic(Characteristic.Model, "S20")
+  .setCharacteristic(Characteristic.SerialNumber, "accf23932f74");
+
+// listen for the "identify" event for this Accessory
+outlet.on('identify', function(paired, callback) {
+  OUTLET.identify();
+  callback(); // success
+});
+
+// Add the actual outlet Service and listen for change events from iOS.
+// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
+outlet
+  .addService(Service.Outlet, "Fake Outlet") // services exposed to the user should have "names" like "Fake Light" for us
+  .getCharacteristic(Characteristic.On)
+  .on('set', function(value, callback) {
+    OUTLET.setPowerOn(value);
+    callback(); // Our fake Outlet is synchronous - this value has been successfully set
+  });
+
+// We want to intercept requests for our current power state so we can query the hardware itself instead of
+// allowing HAP-NodeJS to return the cached Characteristic.value.
+outlet
+  .getService(Service.Outlet)
+  .getCharacteristic(Characteristic.On)
+  .on('get', function(callback) {
+
+    // this event is emitted when you ask Siri directly whether your light is on or not. you might query
+    // the light hardware itself to find this out, then call the callback. But if you take longer than a
+    // few seconds to respond, Siri will give up.
+
+    var err = null; // in case there were any problems
+
+    if (OUTLET.powerOn) {
+      console.log("Are we on? Yes.");
+      callback(err, true);
+    }
+    else {
+      console.log("Are we on? No.");
+      callback(err, false);
+    }
+  }); 
